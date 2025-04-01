0895622dae096bfd61e86937bfdbefa1d0c93610
Neeraj-gagat <neerajgagat9999@gmail.com>
2025-04-02 00:37:54 +0530

    done

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -29,7 +29,7 @@ export const Hero = () => {
         },
     ]
 
-    return <div id="home" style={{backgroundImage: "url('/hero_bg.jpg')"}} className="h-screen bg-cover bg-center">
+    return <div id="home" style={{backgroundImage: "url('/hero_bg5.png')"}} className="h-screen bg-cover bg-center">
         <div className="pl-4 md:pl-10 pt-40 md:pt-30">
         <TypewriterEffectSmooth words={words}/>
         {/* <p className="text-5xl font-bold text-slate-900 max-w-[700px]">&quot;Trusted Visa Experts: Begin Your Journey Today!</p> */}
@@ -42,7 +42,7 @@ export const Hero = () => {
                 
             </div>
         <div className="pt-14 space-x-5 font-sans">
-            <a className="py-3 md:py-5 px-4 md:px-14 text-base md:text-base rounded-lg text-white font-medium bg-[#00CC99] hover:bg-[#0191FF] transition duration-400 " href="#contact us">Book Appointment</a>
+            <a className="py-3 md:py-5 px-4 md:px-14 text-base md:text-base rounded-lg text-white font-medium bg-[#00CC99] hover:bg-[#0191FF] transition duration-400 " href="tel:9888386639">Book Appointment</a>
             <a className="py-3 md:py-5 px-4 md:px-14 text-base md:text-base text-slate-900  rounded-lg hover:text-white font-medium hover:bg-[#00CC99] bg-white transition duration-400 " href="#about">Read Story</a>
         </div>
         </div>
